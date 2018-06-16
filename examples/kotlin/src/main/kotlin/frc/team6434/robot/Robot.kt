6b89b33db8b021491b6623b5d9c212d653d52f61
Illawarra5761 <illawarra5761@gmail.com>
2018-06-16 12:35:29 +1000

    joystick

--- examples/kotlin/src/main/kotlin/frc/team6434/robot/Robot.kt
+++ examples/kotlin/src/main/kotlin/frc/team6434/robot/Robot.kt
@@ -1,9 +1,20 @@
 package frc.team6434.robot
 
+import edu.wpi.first.wpilibj.GenericHID.Hand
 import edu.wpi.first.wpilibj.IterativeRobot
+import edu.wpi.first.wpilibj.XboxController
 
 class Robot : IterativeRobot() {
+    val XBOX_CONTROLLER_PORT = 0
+
+    val controller = XboxController(XBOX_CONTROLLER_PORT)
+
     override fun robotInit() {
         println("Hello World from Kotlin!")
+    }
+
+    override fun teleopPeriodic() {
+      val x = controller.getY(Hand.kRight)
+      val y = controller.getY(Hand.kLeft)
     }
 }
